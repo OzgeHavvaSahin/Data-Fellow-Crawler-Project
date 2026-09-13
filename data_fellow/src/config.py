35d38d04af9e852b22196bbf4485bf97c2791b7c
Özge Havva Şahin <ozgehavva.02@gmail.com>
2026-09-13 18:28:54 +0300

    local static link ile yeni kategori

--- data_fellow/src/config.py
+++ data_fellow/src/config.py
@@ -4,7 +4,8 @@ URLS = {
     "world": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FuUnlHZ0pVVWlnQVAB?hl=tr&gl=TR&ceid=TR%3Atr",
     "sports": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FuUnlHZ0pVVWlnQVAB?hl=tr&gl=TR&ceid=TR%3Atr",
     "business": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FuUnlHZ0pVVWlnQVAB?hl=tr&gl=TR&ceid=TR%3Atr",
-    # "local": "https://news.google.com/topics/CAAqHAgKIhZDQklTQ2pvSWJHOWpZV3hmZGpJb0FBUAE?hl=tr&gl=TR&ceid=TR%3Atr",
+    #static sakarya linki
+    "local": "https://news.google.com/topics/CAAqHAgKIhZDQklTQ2pvSWJHOWpZV3hmZGpJb0FBUAE/sections/CAQiVkNCSVNPam9JYkc5allXeGZkakpDRUd4dlkyRnNYM1l5WDNObFkzUnBiMjV5RFJJTEwyY3ZNVEl4Y21OeU1tNTZEUW9MTDJjdk1USXhjbU55TW00b0FBKjQIACowCAoiKkNCSVNHVG9JYkc5allXeGZkako2RFFvTEwyY3ZNVEl4Y21OeU1tNG9BQVABUAE?hl=tr&gl=TR&ceid=TR%3Atr",
     "health": "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNR3QwTlRFU0FuUnlLQUFQAQ?hl=tr&gl=TR&ceid=TR%3Atr",
     "entertainment": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FuUnlHZ0pVVWlnQVAB?hl=tr&gl=TR&ceid=TR%3Atr",
     "science_and_tech": "https://news.google.com/topics/CAAqKAgKIiJDQkFTRXdvSkwyMHZNR1ptZHpWbUVnSjBjaG9DVkZJb0FBUAE?hl=tr&gl=TR&ceid=TR%3Atr",
